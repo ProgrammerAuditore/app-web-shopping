338fe17495a96d45586be483c76ac0c8e0b082d3
ProgrammerAuditore <victorvj098@gmail.com>
2022-12-03 17:53:54 -0600

    *** dev : fix bug enviroment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 export const PORT = process.env.PORT || 3000
-export const MYSQLUSER = process.env.MYSQL_USERNAME || "root"
-export const MYSQLPASSWORD = process.env.MYSQL_PASSWORD || ""
-export const MYSQLHOST = process.env.MYSQL_HOST || "localhost"
-export const MYSQLPORT = process.env.MYSQL_PORT || 3306
-export const MYSQLDATABASE = process.env.MYSQL_DATABASE || "dbboutique"
+export const MYSQLUSER = process.env.MYSQLUSER || "root"
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || ""
+export const MYSQLHOST = process.env.MYSQLHOST || "localhost"
+export const MYSQLPORT = process.env.MYSQLPORT || 3306
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || "dbboutique"
